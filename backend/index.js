676f53d6c1e35124873c8400e2b5269762bf3880
heyAdin <adinahmad003@gmail.com>
2025-05-21 01:27:43 +0530

    express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const routes = require('./routes/index');
+require('dotenv').config();
+
+app.use(express.json(),cors());
+app.use('/api/v1/',routes);
+
+app.listen(3000,()=>{
+    console.log(`server is listening on port 3000`);
+})
