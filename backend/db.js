35b6b9d07081ca23d3701769116b774f887e3527
heyAdin <adinahmad003@gmail.com>
2025-05-21 01:24:53 +0530

    Added Account Schema

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { type } = require('os');
+const { number } = require('zod');
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URL);
 
@@ -9,6 +11,19 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-const User = mongoose.model('User',userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        require : true
+    },
+    balance :{
+        type : Number,
+        require : true
+    } 
+})
 
-module.exports = {User};
+
+const User = mongoose.model('User',userSchema);
+const Account = mongoose.model('Account',accountSchema);
+module.exports = {User,Account};
