1dc7c36f049b7545b766a53a2f1e1bc96d114d2d
heyAdin <adinahmad003@gmail.com>
2025-05-22 04:02:02 +0530

    Search Bar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,5 @@
+export function SearchBar(){
+    return <div className="m-4 mt-10 rounded-3xl p-2 bg-white/10 ">
+        <input type="text" placeholder="Search users" className="w-full backdrop-blur-md placeholder-white/40 focus:outline-none text-lg text-white px-3"/>
+    </div>
+}
