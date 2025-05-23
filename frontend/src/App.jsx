45f764114f0aa4890b829d05213d83075a9a5cc5
heyAdin <adinahmad003@gmail.com>
2025-05-23 17:08:15 +0530

    wrapped protected pages with protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,33 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
 import { Dashboard } from './pages/Dashboard'
+import { ProtectedRoute } from './components/ProtectedRoutes'
+import { FundsTransfer } from './pages/FundsTransfer'
 function App() {
 
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='bg-gradient-to-br from-[#0A2B24] to-[#176F5B] min-h-screen flex justify-center items-center h-screen'>
       <BrowserRouter>
         <Routes>
-          <Route path='/signin' element={<Suspense fallback={"...Loading"}> <Signin/> </Suspense>} />
-          <Route path='/signup' element={<Suspense fallback={"...Loading"}> <Signup/> </Suspense>} />
-          <Route path='/dashboard' element={<Suspense fallback={"...Loading"}> <Dashboard /> </Suspense>} />
+          <Route path='/signin' element={<Suspense fallback={"...Loading"}> <Signin /> </Suspense>} />
+          <Route path='/signup' element={<Suspense fallback={"...Loading"}> <Signup /> </Suspense>} />
+          <Route path='/' element={<Suspense fallback={"...Loading"}>
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          </Suspense>} />
+
+          <Route path='/dashboard' element={<Suspense fallback={"...Loading"}>
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          </Suspense>} />
+
+          <Route path='/funds-transfer' element={<Suspense>
+            <ProtectedRoute>
+              <FundsTransfer />
+            </ProtectedRoute>
+          </Suspense>} />
         </Routes>
       </BrowserRouter>
     </div>
